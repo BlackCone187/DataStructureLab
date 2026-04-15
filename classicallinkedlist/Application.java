f113c6d03c0fa23e2d2fa3844549924051313786
BlackCone <aldeennoor2007@gmail.com>
2026-04-15 16:49:09 +0300

    Classic LinkedList

---
+++ classicallinkedlist/Application.java
@@ -0,0 +1,15 @@
+package com.example.classicallinkedlist;
+
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+public class Application extends javafx.application.Application {
+    @Override
+    public void start(Stage stage) {
+        Scene scene = new Scene(new BorderPane(), 320, 240);
+        stage.setTitle("Hello!");
+        stage.setScene(scene);
+        stage.show();
+    }
+}
